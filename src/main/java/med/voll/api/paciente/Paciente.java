8ba9bd8df9e50b148718ade22dd443920d34fd95
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-04-28 16:14:09 -0300

    Criado listagem de pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -25,7 +25,10 @@ public class Paciente {
 
     @Embedded
     private Endereco endereco;
+
+    private Boolean ativo;
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
